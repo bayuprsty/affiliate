7c0917917b2e541850784451771392453c4e45ab
bayuprsty <bayuprsty24@gmail.com>
2020-12-15 14:19:47 +0700

    - remove vendor seeder from DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
-        $this->call(VendorTableSeeder::class);
         $this->call(CommissionTypeTableSeeder::class);
         $this->call(MediaSeeder::class);
         $this->call(WithdrawalStatusSeeder::class);
